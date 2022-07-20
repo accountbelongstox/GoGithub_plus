a5c7fce6c2e3e0802b2b33ab6d713416423c66ae
accountbelongstox@163.com <accountbelongstox@163.com>
2022-07-21 00:40:20 +0800

    new amend

--- push_gitee_and_github.bat
+++ push_gitee_and_github.bat
@@ -1,9 +1,10 @@
 git add .
-git commit -m "new amend"
-git remote add gitee git@gitee.com:accountbelongstox/b2bwork.baidu.git
 git branch -M 'master'
-git push -u gitee master
-git remote add github git@github.com:accountbelongstox/b2bwork.baidu.git
+git commit -m "new amend"
+git remote add gitee git@gitee.com:accountbelongstox/GoGitHub_plus.git
+git push -u gitee master --force
+git remote add github git@github.com:accountbelongstox/GoGithub_plus.git
 git branch -M 'main'
-git push -u github main
+git commit -m "new amend"
+git push -u github main --force
 cmd
